5b23cc97bdbd845b3bb230e110e72b2b14a5f1ac
Gabby <emailgabby@gmail.com>
2018-02-23 16:19:55 -0500

    hi jordan

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -14,7 +14,7 @@ char forward(char c) {
 	else if (c == 'z') {
 		c = 'a';
 	}
-	else if (c < 101 || c > 172) { // c is a non letter
+	else if (c < 65 || c > 123) { // c is a non letter
 		return c;
 	}
 	else {
@@ -23,36 +23,29 @@ char forward(char c) {
 	return c;
 }
 
-char rev(char c) {
-	if (c == 'A') {
-		c = 'Z';
-	}
-	else if (c == 'a') {
-		c = 'z';
-	}
-	else if (c < 101 || c > 172) { // c is a non-letter
-		return c;
-	}
-	else {
-		c--;
-	}
-	return c;
-}
-
 int main(int argc, char *argv[]) { // take in cmd line args
+	if (argc <= 1) {
+		my_str("Usage: ./pipes message");
+		return 1;
+	}
+
 	pid_t pid;
 	int parent[2];
 	int child[2];
-	int gchild[2];
+	//int gchild[2];
 	pipe(parent);
 	pipe(child);
 
 	if ( (pid = fork()) < 0) {
 		perror("fork didnt work"), exit(1);
-	} else if (pid == 0) { // child
+	} else if (pid == 0) { // child process
+		/*
+		pid_t gpid;
+		gpid = fork();
 		pipe(gchild);
-	else {
-
+		*/
+	else { // parent process
+		// send cmd line args as a string to child
 	}
 	return 0;
 }

b0cd0d4918edaa6dbec77bbc1459e56325a322a7
Gabby Padriga <emailgabby@gmail.com>
2018-02-23 16:56:10 -0500

    Update pipes.c

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -26,7 +26,7 @@ char forward(char c) {
 
 int main(int argc, char *argv[]) { // take in cmd line args
 	if (argc <= 1) {
-		my_str("Usage: ./pipes message");
+		my_str("Usage: ./pipes [message goes here]");
 		return 1;
 	}
 
@@ -46,6 +46,10 @@ int main(int argc, char *argv[]) { // take in cmd line args
 		}
 		else if (gpid == 0){ // fork went ok
 			// read string from child
+			char gin[100] = "";
+			while (read(child[0], gin, 100) > 0) {
+				my_revstr(gin);
+			}
 			// reverse string and print it
 		}
 		else 
@@ -56,73 +60,7 @@ int main(int argc, char *argv[]) { // take in cmd line args
 		}
 		char send[100] = in;
 		// forward in
-		write 
-
-	}
-	else { // parent process
-		// convert cmd line args to string using vect2str
-		// send strings to child
-	}
-	return 0; // yay u did it
-}#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include "my.h"
-
-/*
-need to import my library to print and add my_vect2str.c
-*/
-
-char forward(char c) {
-	if (c == 'Z') {
-		c = 'A';
-	}
-	else if (c == 'z') {
-		c = 'a';
-	}
-	else if (c < 65 || c > 123) { // c is a non letter
-		return c;
-	}
-	else {
-		c++;
-	}
-	return c;
-}
-
-int main(int argc, char *argv[]) { // take in cmd line args
-	if (argc <= 1) {
-		my_str("Usage: ./pipes message");
-		return 1;
-	}
-
-	pid_t pid;
-	int parent[2];
-	int child[2];
-	pipe(parent); // pipe 1
-	pipe(child); // pipe 2
-
-	if ( (pid = fork()) < 0) {
-		perror("Child fork didn't work"), exit(1);
-	} else if (pid == 0) { // child process
-		/*
-		pid_t gpid;
-		if ( (gpid = fork()) < 0) {
-			perror("Grandchild fork didn't work"), exit(1);
-		}
-		else if (gpid == 0){ // fork went ok
-			// read string from child
-			// reverse string and print it
-		}
-		else 
-		*/
-		char in[100] = "";
-		while (read(parent[0], in, 100) > 0) {
-			forward(in);
-		}
-		char send[100] = in;
-		// forward in
-		write 
+		write(child[1], send, 100);
 
 	}
 	else { // parent process

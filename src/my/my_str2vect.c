ac83f1450f472f31212c25a8fbbcf8574ca20769
Jordan <vrjordant@gmail.com>
2018-03-03 20:42:49 -0500

    fixed my_str2vect

--- src/my/my_str2vect.c
+++ src/my/my_str2vect.c
@@ -5,6 +5,8 @@ char** my_str2vect(char* str) {
 	if (str) {
 		char* temp = str;
 		int count = 0;
+
+		// counts the number of words you need to allocate
 		while (*temp != '\0') {
 			while (*temp == ' ' || *temp == '\t' || *temp == '\n') { // you hit a space
 				temp++;
@@ -17,10 +19,12 @@ char** my_str2vect(char* str) {
 				temp++;
 			}
 		}
-		printf("%s\n", "done counting number of words");
-		printf("%d\n", count);
-		char ** vect = (char**) malloc((count+1)*sizeof(str));
+		//printf("%s\n", "done counting number of words");
+		//printf("%d\n", count);
+		char ** vect = (char**) malloc((count+1)*sizeof(char*));
 		int lcount = 0;
+
+		// copies each string into the result vector
 		for (int i = 0; i<count; i++) {
 			while (*str == ' ' || *str == '\t' || *str == '\n') {
 				str++;
@@ -31,10 +35,14 @@ char** my_str2vect(char* str) {
 				lcount++;
 				temp++;
 			}
-			printf("%s\n", "we get before my_strncpy");
-			printf("%s\n", str);
-			vect[i]=my_strdup(str);
-			printf("%s\n", vect[i]);
+			//printf("%s\n", "we get before my_strncpy");
+			//printf("%s\n", str);
+			
+			char* element = (char*) malloc((lcount+1) * sizeof(char));
+			my_strncpy(element, str, lcount);
+			vect[i] = element;
+
+			//printf("%s\n", vect[i]);
 			str = temp;
 		}
 		vect[count+1] = NULL;
--- test/test_my/test_mystr2vect.c
+++ test/test_my/test_mystr2vect.c
@@ -1,9 +1,56 @@
 #include "my.h"
 #include <stdio.h>
 
+void shakespeare() {
+	char** list = my_str2vect("My bounty is as boundless as the sea, \
+	My love as deep; the more I give to thee, \
+	The more I have, for both are infinite.");
+	for (int i = 0; i < 26; i++) {
+		printf("%s\n", list[i]);
+	}
+	if (list[26] == NULL) {
+		printf("It's null\n");
+	}
+	else {
+		printf("not null\n");
+	}
+}
+
+void null() {
+	char** list = my_str2vect(NULL);
+	if (list == NULL) {
+		printf("null\n");
+	}
+	else {
+		printf("not null\n");
+	}
+}
+
+void empty() {
+	char** list = my_str2vect("");
+	if (list == NULL) {
+		printf("list is null\n");
+	}
+	else if (list[0] == NULL) {
+		printf("first elem is null\n");
+	}
+	else {
+		printf("<%s>\n", list[0]);
+	}
+}
+
 int main() {
-	char** array = my_str2vect("Hello world");
-	printf("%s\n", array[0]);
-	printf("%s\n", array[1]);
-	printf("%s\n", array[2]);
+	// char** array = my_str2vect("Hello world");
+	// printf("%s\n", array[0]);
+	// printf("%s\n", array[1]);
+	// if (array[2] == NULL) {
+	// 	printf("NULL\n");
+	// }
+	// else {
+	// 	printf("not null\n");
+	// }
+	//shakespeare();
+	//null();
+	empty();
+	return 0;
 }

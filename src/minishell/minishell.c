68efc96ba6359fc9a2b0365b0dbe0a54fe235298
Gabby <emailgabby@gmail.com>
2018-03-05 14:47:43 -0500

    fixed ls in minishell

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include "my.h"
+#include <signal.h>
+#include <sys/wait.h>
 
 /*	Gabrielle Padriga and Jordan Tantuico
 	We pledge our honor that we have abided by the Stevens Honor System
@@ -17,7 +18,11 @@ Things that may be useful:
 		argv:  is a null terminated array of character pointers.
 */
 
+void doNothing(int n) { }
+
 int main() {
+	// signals definition
+	signal(SIGINT, doNothing);
 	while (1) {
 		// print prompt and current working directory
 		char cwd[1024];
@@ -41,35 +46,44 @@ int main() {
 			seen++;
 			read(0, input, 1);
 		}
-		my_str("this is what's in our directions string: ");
-		my_str(begin);
-		my_str("\n");
+		//my_str("this is what's in our directions string: ");
+		//my_str(begin);
+		//my_str("\n");
 		char ** dir = my_str2vect(begin);
 		int sizeDir = my_vectsize(dir);
 
+		// if nothing is entered
 		if (sizeDir == 0) {
 			my_str("Please enter something, type help if you don't know anything\n");
 		}
+
 		// cd command
 		else if (my_strcmp(dir[0], "cd") == 0) { // cd somepath
 			if (chdir(dir[1]) == -1) {
 				my_str("Either that directory doesn't exist, or you're not allowed to access it\n");
 			}
 		}
+
 		// ls command
 		else if (my_strcmp(dir[0], "ls") == 0 && sizeDir == 1) {
-			int countFiles;
-			struct dirent **tempFiles;
-			countFiles = scandir(".", &tempFiles, NULL, alphasort);
-			while (countFiles-- && (countFiles > 1)) { // from the man pages
-				my_str(tempFiles[countFiles]->d_name);
-				my_str(" ");
-				free(tempFiles[countFiles]);
+			pid_t pid;
+			if ((pid = fork()) < 0) {
+				perror("Child fork didn't work\n"), exit(1);
 			}
-			my_str("\n");
-			free(tempFiles);
+			else if (pid == 0) { // child process
+				const char* cmd = "ls";
+				char *arr[2];
+				arr[0]= "ls";
+				arr[1]= NULL;
+				execvp(cmd,arr);
+				exit(0);
+			}
+			else { // parent
+				wait(NULL);
+			}
 		}
 
+		// help
 		else if (my_strcmp(dir[0], "help") == 0 && sizeDir == 1) {
 			my_str("Here are things you can do: \n");
 			my_str("cd [directory] : Changes current working directory to what's specified\n");
@@ -79,25 +93,38 @@ int main() {
 			my_str("help : You're already here!\n");
 		}
 
+		// exit
 		else if (my_strcmp(dir[0], "exit") == 0 && sizeDir == 1) {
 			my_str("That's all folks!\n");
 			return 1;
 		}
 
+		// exec function
 		else if (my_strcmp(dir[0], "./idunnoyet") == 0) { // I dunno how we're gonna implement this {
 			//use execvp and str2vect to pass args
 			// fork before execvp
-			my_str("Not implemented yet");
+			pid_t pid;
+			if ((pid = fork()) < 0) {
+				perror("Child fork didn't work\n"), exit(1);
+			}
+			else if (pid == 0) { // child process
+
+			}
 		}
 
+		// no command matches or trying to exec
 		else {
-			my_str("What does that even mean dude?\n");
+			char ** cmnds = (dir)++; // how to cut off first word in vector?
+			my_str(cmnds[0]);
+			if (execvp(dir[0], cmnds) == -1) {
+				my_str("Ya can't do that!\n");
+			}
 		}
+		/* we don't need to worry about memory freeing apparently
 		for (int i = 0; i < sizeDir; i++) {
 			my_str(dir[i]);
 			my_str(" ");
 		}
-		/*
 		for (int i = 0; i <= sizeDir; i++) {
 			printf("%s\n", "freeing ");
 			printf("%d\n", i);
